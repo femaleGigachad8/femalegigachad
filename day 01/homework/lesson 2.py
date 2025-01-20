db437e344f0ced6b4589f91c4b2f121c9092a09b
femaleGigachad8 <im.sigma.sui7@gmail.com>
2025-01-20 16:40:04 +0400

    first commit

---
+++ day 01/homework/lesson 2.py
@@ -0,0 +1,19 @@
+name = "my name is tekla."
+surname = "my surname is marjanidze."
+# name aris cvladi 
+# = aris cvladistvis mnishvnelobis mimniwebeli simbolo 
+# "tekla" aris cvladistvis mnishvneloba
+# print (name) 
+# print funqcias gadaecema ekranze gamosatani obieqti
+#name = "tekla" # es aris stringi. str tipis cvladi
+age = "i'm 13yrs old." #integeri mteli ricxvi.
+height = "i am 150cm tall." #es aris float tipis cvladi (atwiladi)
+#boolean bool tipis cvladi
+knows_programming = True #sheidzleba iyos false c
+print (name + " " + surname + " " + age + " " + height)
+
+
+
+
+
+
