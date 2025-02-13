47ad433b00f9bcf4de93ca3378f9e43f49a3a25d
femaleGigachad8 <im.sigma.sui7@gmail.com>
2025-02-13 23:26:20 +0400

    commit

---
+++ day 02/homework/friends.py
@@ -0,0 +1,10 @@
+# დავიმატე შემდეგი გუნდელები fb-ზე:
+
+#Anano Tsagareishvili
+#ჯან შაჰიმბაში 
+#Ioane Dumbadze 
+#Dato Datebashvili 
+#Saba Zardiashvili 
+#Nika Gudushauri 
+
+#ამ ბავშვებს გავუგზავნე მეგობრობის მოთხოვნა. იმედია დამტანხმდენიან და ვიმეგობრებთ <3
