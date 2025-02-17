a26f1828de244b1c2a6b5f6b58eed0121c8a86fd
femaleGigachad8 <im.sigma.sui7@gmail.com>
2025-02-17 17:37:57 +0400

    commit

---
+++ day 03/homework/3.py
@@ -0,0 +1,11 @@
+#3) შექმენით ცვლადი, შეუცვალეთ მას მნიშვნელობა 3-ჯერ. 
+#თითოეული მნიშვნელობის შეცვლის შემდეგ დაბეჭდეთ ეს ცვლადი.
+
+
+name = "GOA"
+name = "the best academy"
+print (name)
+name = "GOA is the best"
+print (name)
+name = "i really like GOA"
+print (name)
---
+++ day 03/homework/4.py
@@ -0,0 +1,9 @@
+# 4) შექმენით 5 ცვლადი, სადაც შეინახავთ რიცხვებს. დაბეჭდეთ მათი ჯამი print ფუნქციის გამოყენებით.
+age = 13
+cost = 107
+number = 30
+children = 27
+books = 3
+print (age+cost+number+children+books)
+#or
+print (13+107+30+27+3)
