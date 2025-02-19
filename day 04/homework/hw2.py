f76ba0acb6ab3f7f25d77b7209f48a7251c0bbf7
femaleGigachad8 <im.sigma.sui7@gmail.com>
2025-02-19 21:17:10 +0400

    commit

---
+++ day 04/homework/hw2.py
@@ -0,0 +1,10 @@
+#2) შექმენით 3 ცვლადი სადაც შეინახავთ თქვენთვის სასურველ მონაცემს. 
+#type ფუნქციის გამოყენებით დაბეჭდეთ მათი ტიპი.
+
+car = "porsche spyder 918"
+horsepower =  887 
+rapper = "EMINEM"
+
+print (type (car))
+print (type (horsepower))
+print (type (rapper))
