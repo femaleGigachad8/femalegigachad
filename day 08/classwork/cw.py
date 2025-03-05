abe332d3f935512ae4db79a8dfa839694b9c4bc3
femaleGigachad8 <im.sigma.sui7@gmail.com>
2025-03-05 19:29:08 +0400

    commit

---
+++ day 08/classwork/cw.py
@@ -0,0 +1,28 @@
+# 1) Name = "Nika"
+# naMe = "Luka"
+# NAME = "Data"
+# naME = "Vano"
+# name = "Demetre"
+# NaMe = "Jan"
+# name = "Giorgi"
+
+# რამდენი ცვლადი შეიქმნება ამ კოდის მიხედვით?(აღწერეთ რატომ)
+
+#ამ კოდის მიხედვით შეიქმნება 6 ცვლადი რადგან python არის case-sensitive პროგრამირების ენა და ყველა სიმბოლოს 
+# კითხულობს განსხვავებულად. ასევე 7 დაწერილი კოდიდან 6 იქნება რადგან მეშვიდე ხაზზე მოხდა რეინიციალიზება.
+
+
+
+# 2) შექმენით 5 ცვლადი, snake_case-ის გამოყენებით და დაბეჭდეთ
+
+fav_subject = "math"
+Fav_subject = "physics"
+Fav_subject1 = "georgian"
+fav_colour = "purple"
+fav_car = "porsche"
+
+print (fav_subject)
+print (Fav_subject)
+print (Fav_subject1)
+print (fav_colour)
+print (fav_car)
