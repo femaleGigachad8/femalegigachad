20dde434688db3ffbb60991a49f4089e86c4d89f
femaleGigachad8 <im.sigma.sui7@gmail.com>
2025-05-03 11:40:23 +0400

    commit

---
+++ day 24/homework/hw.py
@@ -0,0 +1,12 @@
+#2) მომხმარებელს შემოატანინე 5 რიცხვი და დაბეჭდეთ მათი ნამრავლი
+
+i = 0 
+result = 1
+
+while i < 5:
+    num = int(input("Please enter a number: "))
+    result = result * num
+    i = i + 1
+print ("The multiplication of the 5 numbers is : " , result)
+
+
